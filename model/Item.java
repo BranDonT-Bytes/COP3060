bbd3c851c929870a11a62b6a5ffbb3772d553576
BranDonT-Bytes <brandon12.brown@famu.edu>
2025-11-25 11:01:03 -0500

    Create Item.java

---
+++ model/Item.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+public class Item {
+    private Long id;
+    private String name;
+
+    public Item() {}
+    public Item(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+}
